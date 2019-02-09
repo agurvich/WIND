fixed initial state and equilibrium abundance calculation

python/eqm_eqns.py
@@ -1,6 +1,6 @@
-import numpy
+import numpy as np
 
-def get_eqm_densities(DENSITY,TEMP,helium_fraction):
+def get_eqm_densities(DENSITY,TEMP,y_helium):
     constants_dict = {}
     constants_dict['Gamma_(e,H0)'] = 5.85e-11 * np.sqrt(TEMP)/(1+(TEMP/1e5)) * np.exp(-157809.1/TEMP)
     constants_dict['Gamma_(gamma,H0)'] = 4.4e-11
@@ -16,12 +16,14 @@ def get_eqm_densities(DENSITY,TEMP,helium_fraction):
 
     densities = np.zeros(5)
     ne = DENSITY
-    for i in range(10):
-        ne = sub_get_densities(constants_dict,DENSITY,TEMP,densities,ne,helium_fraction) 
+    for i in range(1000):
+        ne = sub_get_densities(constants_dict,DENSITY,TEMP,densities,ne,y_helium) 
+        if (not i%100):
+            print(densities)
     return densities
 
 
-def sub_get_densities(constants_dict,DENSITY,TEMP,densities,ne,helium_fraction):
+def sub_get_densities(constants_dict,DENSITY,TEMP,densities,ne,y_helium):
     """ from eqns 33 - 38 of Katz96"""
     
     ## H0
@@ -30,13 +32,14 @@ def sub_get_densities(constants_dict,DENSITY,TEMP,densities,ne,helium_fraction):
         (constants_dict['alpha_(H+)'] + 
             constants_dict['Gamma_(e,H0)'] + 
             constants_dict['Gamma_(gamma,H0)']/ne)
+    )
 
     ## H+
     densities[1] = DENSITY - densities[0]
 
     ## He+
     densities[3] = (
-        DENSITY * helium_fraction  / 
+        DENSITY * y_helium  / 
         (1 + (
             (constants_dict['alpha_(He+)'] + constants_dict['alpha_(d)']) / 
             (constants_dict['Gamma_(e,He0)']+constants_dict['Gamma_(gamma,He0)']/ne)

python/python_harness.py
@@ -100,6 +100,7 @@ Nsystems = 2
 Nequations_per_system = 5
 TEMP = 1e2 ## K
 DENSITY = 1e2 ## 1/cm^-3
+y_helium = 0.25
 
 constants_dict = {}
 
@@ -148,18 +149,18 @@ def get_constants(TEMP,Nsystems):
     ##return np.array(list(range(10))*Nsystems).astype(np.float32)
     return (constants*3.15e7).astype(np.float32) ## convert to 1/yr
 
-def initialize_equations(density,Nsystems):
+def initialize_equations(density,Nsystems,y_helium):
     return np.array([
-    0.25, ## H0
-    0.25, ## H+
-    0.25, ## He0
-    0.125,## He+
-    0.125 ## He++
+    0.5, ## H0
+    0.5, ## H+
+    0.5*y_helium/4., ## He0
+    0.25*y_helium/4.,## He+
+    0.25*y_helium/4. ## He++
     ]*Nsystems).astype(np.float32)*density
 
 """
 constants = get_constants(TEMP,Nsystems)
-equations = initialize_equations(DENSITY,Nsystems)
+equations = initialize_equations(DENSITY,Nsystems,y_helium)
 
 runIntegratorOutput(
     c_cudaIntegrateEuler,'RK2',
@@ -174,7 +175,7 @@ print("---------------------------------------------------")
 """
 
 constants = get_constants(TEMP,Nsystems)
-equations = initialize_equations(DENSITY,Nsystems)
+equations = initialize_equations(DENSITY,Nsystems,y_helium)
 
 runIntegratorOutput(
     c_cudaSIE_integrate,'SIE',
@@ -189,9 +190,7 @@ runIntegratorOutput(
 print("---------------------------------------------------")
 
 constants = get_constants(TEMP,Nsystems)
-equations = initialize_equations(DENSITY,Nsystems)
-print(1-np.sum(equations[:5])/DENSITY)
-eqm_densities = get_eqm_densities(DENSITY,TEMP,1-np.sum(equations[:5])/DENSITY)
+equations = initialize_equations(DENSITY,Nsystems,y_helium)
 
 runIntegratorOutput(
     c_cudaBDF2_integrate,'BDF2',
@@ -202,6 +201,8 @@ runIntegratorOutput(
     Nequations_per_system,
     output_mode = 'a')
 
+print(y_helium)
+eqm_densities = get_eqm_densities(DENSITY,TEMP,y_helium)
 with h5py.File("katz96_out.hdf5",'a') as handle:
     group = handle.attrs['eqm_densities'] = eqm_densities
 