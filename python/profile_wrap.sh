4172b6a06c44c9c22be80e7d3543dc66d96f50d4
Alex Gurvich <agurvich@u.northwestern.edu>
2019-03-08 16:53:50 -0600

    add memory profiling, see github.com/agurvich/shell_scripts for device_memory_profile definition, uses nvidia-smi --query-device -lms --format=csv to output memory usage statistics.

--- python/profile_wrap.sh
+++ python/profile_wrap.sh
@@ -9,4 +9,9 @@ NAME=SIE
 
 cd /u/sciteam/gurvich/wind/python
 rm nvvp_profiles/${NAME}*
+
+## startup nvidia-smi in the background
+device_memory_profile "nvvp_profiles/${NAME}_memory.csv" & 
 nvprof -o nvvp_profiles/${NAME}.%p.out --print-gpu-trace --profile-all-processes & python python_harness.py
+## kill background nvidia-smi process
+killall nvidia-smi
