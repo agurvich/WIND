6e1ea0029776d201890a65e0fe1d2c4b29c413d6
Alex Gurvich <agurvich@u.northwestern.edu>
2019-05-06 12:43:29 -0500

    add BW  profiling wrappers

---
+++ python/profiling_tools/wrapper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export ATP_ENABLED=1 # or setenv ATP_ENABLED 1
+
+cd /u/sciteam/gurvich/wind/python
+python python_harness.py
---
+++ python/shell_scripts/memory_ntile_make_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+    
+## $1 (Katz96 or NR_test) $2 integer number of tilings
+##  any other arguments are passed to python_harness.py
+
+## make necessary precompile files and build
+bash shell_scripts/precompile_system.sh $1 $2 
+
+bash profiling_tools/memory_profile.sh "${@:1}"
+
+bash shell_scripts/move_debug_file.sh $1 $2 
+
