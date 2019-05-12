f8946e3f21a831be74b7dcb8202745b49492e6b2
Alex Gurvich <agurvich@u.northwestern.edu>
2019-05-12 16:57:24 -0500

    first attempt at SIE as independent blocks. it compiles at least

--- cuda/include/device.h
+++ cuda/include/device.h
@@ -2,3 +2,11 @@ __device__ float calculate_dydt(
     float ,//tnow,
     float *, //constants,
     float * );//equations);
+
+
+__device__ void calculate_jacobian(
+    float ,//tnow,
+    float *,// constants,
+    float *,// shared_temp_equations,
+    float *);// Jacobian)
+;
--- cuda/include/linear_algebra.h
+++ cuda/include/linear_algebra.h
@@ -1,7 +1,10 @@
-#include <cublas_v2.h>
-
 __global__ void gjeInvertMatrixBatched(
     float *, // d_matricess_flat,
     float *, // d_inversess_flat,
     int ,//Ndim,
     int );//Nbatch)
+
+__device__ void gjeInvertMatrix(
+    float *, // d_matrix
+    float *, // d_inverse
+    int); // Ndim
