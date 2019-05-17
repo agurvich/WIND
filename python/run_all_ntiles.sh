66158b75cbd21e5ef4696ff2f619281eae009253
Alex Gurvich <agurvich@u.northwestern.edu>
2019-05-17 15:52:41 -0500

    update precompile step to be able to set absolute/relative tolerance

--- python/run_all_ntiles.sh
+++ python/run_all_ntiles.sh
@@ -1,29 +1,53 @@
 #!/bin/bash
 
 SYSTEM_NAME=Katz96
-Ntiles=(1 5 10 15 20 25 30 40 45 50 100 200 500)
-Nsystem_tiles=(1) #(1 5 10 15 20 25 30 40 45 50 100 200 500)
+Ntiles=(30) #(1 5 10 15 20 25 30 40 45 50 100 200 500)
+Nsystem_tiles=(1) # 5 10 20 50 100 200 500 1000) # (1) #
 Nstepss=(1 5 10 15 20 25 30 40 45 50 100 200 500)
 
 ## have to recompile in fixed step mode
 n_integration_steps=1
 
+absolutes=('5e-3')
+relatives=('5e-3')
 
-for Ntile in "${Ntiles[@]}"
+for ABSOLUTE in "${absolutes[@]}"
 do
-    for Nsystem_tile in "${Nsystem_tiles[@]}"
+    for RELATIVE in "${relatives[@]}"
     do
-        NAME=${SYSTEM_NAME}_neqntile_${Ntile}_nsystemtile_${Nsystem_tile}_fixed_${n_integration_steps}
-        DATADIR=../data/${NAME}
-        if [ ! -d ${DATADIR} ]
+        ## make the final directory for all the files in this grid
+        maindata=../data/data_${ABSOLUTE:0:2}${ABSOLUTE:3}_${RELATIVE:0:2}${RELATIVE:3}
+        if [ ! -d ${maindata} ]
             then
-            ## only need to compile for different tilings that affect # of equations/system
-            bash shell_scripts/precompile_system.sh ${SYSTEM_NAME} ${Ntile} ${Nsystem_tile} ${n_integration_steps}
-            echo  ${NAME} not found!
-            bash profiling_tools/memory_profile.sh ${SYSTEM_NAME} ${Ntile} ${Nsystem_tile} ${n_integration_steps} --SIE=True "${@:2}"
-            bash profiling_tools/memory_profile.sh ${SYSTEM_NAME} ${Ntile} ${Nsystem_tile} ${n_integration_steps} --RK2=True "${@:2}"
-            bash profiling_tools/python_memory_profile.sh ${SYSTEM_NAME} ${Ntile} ${Nsystem_tile} ${n_integration_steps} --SIE=True --gold=True "${@:2}"
-            bash profiling_tools/python_memory_profile.sh ${SYSTEM_NAME} ${Ntile} ${Nsystem_tile} ${n_integration_steps} --RK2=True --gold=True "${@:2}"
+            mkdir ${maindata}
         fi
-    done
-done
+
+        ## compile the support files as well as the common files-- second true makes clean
+        bash shell_scripts/precompile_system.sh ${SYSTEM_NAME} ${Ntiles[0]} ${Nsystem_tiles[0]} ${n_integration_steps} true  true ${ABSOLUTE} ${RELATIVE}
+        for Ntile in "${Ntiles[@]}"
+        do
+            ## compile the new system for this Neqn_tile
+            bash shell_scripts/precompile_system.sh ${SYSTEM_NAME} ${Ntile} ${Nsystem_tiles[0]} ${n_integration_steps} true  false ${ABSOLUTE} ${RELATIVE}
+            for Nsystem_tile in "${Nsystem_tiles[@]}"
+            do
+                NAME=${SYSTEM_NAME}_neqntile_${Ntile}_nsystemtile_${Nsystem_tile}_fixed_${n_integration_steps} 
+                DATADIR=${maindata}/${NAME}
+                if [ ! -d ${DATADIR} ]
+                    then
+                    bash shell_scripts/precompile_system.sh ${SYSTEM_NAME} ${Ntile} ${Nsystem_tile} ${n_integration_steps} false false ${ABSOLUTE} ${RELATIVE}
+                    echo  ${NAME} not found in ${DATADIR}
+                    ## run SIE on the gpu with memory profiling
+                    bash profiling_tools/memory_profile.sh ${SYSTEM_NAME} ${Ntile} ${Nsystem_tile} ${n_integration_steps} --SIE=True "${@:2}"
+                    ## run RK2 on the gpu with memory profiling
+                    bash profiling_tools/memory_profile.sh ${SYSTEM_NAME} ${Ntile} ${Nsystem_tile} ${n_integration_steps} --RK2=True "${@:2}"
+                    ## run SIE on the cpu with memory profiling
+                    bash profiling_tools/python_memory_profile.sh ${SYSTEM_NAME} ${Ntile} ${Nsystem_tile} ${n_integration_steps} --SIE=True --gold=True "${@:2}"
+                    ## run RK2 on the cpu with memory profiling
+                    bash profiling_tools/python_memory_profile.sh ${SYSTEM_NAME} ${Ntile} ${Nsystem_tile} ${n_integration_steps} --RK2=True --gold=True "${@:2}"
+                    ## move this file to where it belongs
+                    mv ../data/${NAME} ${maindata}
+                fi
+            done ## for Nsystem_tile
+        done ## for Ntile
+    done ## for RELATIVE
+done ## for ABSOLUTE
--- python/shell_scripts/precompile_system.sh
+++ python/shell_scripts/precompile_system.sh
@@ -6,15 +6,28 @@ DATADIR=../data/${NAME}
 echo "Compiling files for" $1 "with" $2 "eqn tiling" $3 "system tiling" $4 "integration steps"
 python wind_harness.py --system_name=${1} --Ntile=${2} --Nsystem_tile=${3} --n_integration_steps=${4} --dumpDebug=False # -dumpDebug=True "${@:5}"
 
-cp `pwd`/${DATADIR}/precompile_device_dydt.cu ../cuda/ode_system
-cp `pwd`/${DATADIR}/precompile_ode_system.cu ../cuda/ode_system
-cp `pwd`/${DATADIR}/precompile_ode_gold.c ../cuda/ode_system
-cd ../cuda
-pwd
-## build the new ODE system and link to the solver .so 's
-make ODEOBJS="ode_system/precompile_ode_system.o ode_system/precompile_device_dydt.o" GOLDODEOBJS="../ode_system/precompile_ode_gold.o"
-cd ../python
+## passed flag whether we should rebuild or not
+if [ ${5} == true ]
+    then
+    cp `pwd`/${DATADIR}/precompile_device_dydt.cu ../cuda/ode_system
+    cp `pwd`/${DATADIR}/precompile_ode_system.cu ../cuda/ode_system
+    cp `pwd`/${DATADIR}/precompile_ode_gold.c ../cuda/ode_system
+    cd ../cuda
+    pwd
+    if [ ${6} == true ]
+        then
+        echo "Making clean..."
+        make clean > /dev/null 2>&1
+        make preclean > /dev/null 2>&1
+        echo "...done"
+    fi 
+    ## build the new ODE system and link to the solver .so 's
+    echo "Making..."
+    make ODEOBJS="ode_system/precompile_ode_system.o ode_system/precompile_device_dydt.o" GOLDODEOBJS="../ode_system/precompile_ode_gold.o" ABSOLUTE=${7} RELATIVE=${8} > /dev/null 2>&1
+    echo "...done"
+    cd ../python
+fi
 
 ## move the debug file that's generated to the corresponding debug directories
 ##  and make those
-bash shell_scripts/move_debug_file.sh "${@:1}"
+#bash shell_scripts/move_debug_file.sh "${@:1}"
