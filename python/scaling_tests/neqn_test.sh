304062dd0ec948a023b9cdc62cae7ef95d59d50f
Alex Gurvich <agurvich@u.northwestern.edu>
2019-05-25 15:12:40 -0500

    reduce number of equations scaling, do not need 500 tiling to get best fit

--- python/scaling_tests/neqn_test.sh
+++ python/scaling_tests/neqn_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SYSTEM_NAME=Katz96
-Ntiles=(1 5 10 15 20 25 30 40 45 50 100 200 500)
+Ntiles=(1 5 10 15 20 25 30 40 45 50 100 200)
 Nsystem_tiles=(20) # 5 10 20 50 100 200 500 1000) # (1) #
 
 ## have to recompile in fixed step mode
