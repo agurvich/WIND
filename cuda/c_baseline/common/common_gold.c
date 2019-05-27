6b80539686f80ab776f9720c23ed7da36ac116c2
Alex Gurvich <agurvich@u.northwestern.edu>
2019-05-26 21:30:46 -0500

    create multiple versions of common gold with ifdefs instead of making different functions all the time

--- cuda/c_baseline/common/common_gold.c
+++ cuda/c_baseline/common/common_gold.c
@@ -5,6 +5,46 @@
 
 #include "common_gold.h"
 
+int checkError(
+    float * y1,
+    float * y2,
+    int Neqn_p_sys,
+    float ABSOLUTE, 
+    float RELATIVE){
+
+    double abs_error;
+    double rel_error;
+    int error_flag = 0;
+    for (int eqn_i=0; eqn_i<Neqn_p_sys; eqn_i++){
+        abs_error = fabs(y2[eqn_i]-y1[eqn_i]);
+        if (abs_error >= ABSOLUTE){
+#ifdef LOUD
+            printf("%d absolute failed: %.2e\n",eqn_i,abs_error);
+#endif
+            error_flag = 1;
+        }
+        //if (fabs(y1[eqn_i]) > ABSOLUTE && 
+            //fabs(y2[eqn_i]) > ABSOLUTE){
+            //rel_error = abs_error/fmin(fabs(y1[eqn_i]),fabs(y2[eqn_i]));
+#if RK2
+        rel_error = fabs((y2[eqn_i] - y1[eqn_i])/(2*y2[eqn_i]-y1[eqn_i]+1e-12));
+#else
+        rel_error = fabs((y2[eqn_i] - y1[eqn_i])/(y2[eqn_i]+1e-12));
+#endif
+
+        if (rel_error >= RELATIVE && 
+            fabs(y1[eqn_i]) > ABSOLUTE &&
+            fabs(y2[eqn_i]) > ABSOLUTE){
+#ifdef LOUD
+                printf("%d relative failed: %.2e\n",eqn_i,rel_error);
+#endif
+                error_flag = 1;
+        }// if rel_error >=RELATIVE
+        //}// if fabs(y1) > ABS_TOL && fabs(y2) > ABS_TOL
+    }// for eqn_i <Neqn_p_sys
+    return error_flag;
+}// int checkError
+
 int integrateSystem(
     float tnow,
     float tend,
@@ -99,7 +139,11 @@ int integrateSystem(
 
 
             // get a little more ambitious
-            timestep*=2.0;
+#ifdef RK2
+            timestep*=2;
+#else
+            timestep=(tend-tnow);
+#endif
             unsolved =0;
         }
     }
