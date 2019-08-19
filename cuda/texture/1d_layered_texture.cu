35a034b856c64f544f94b21abcfb742c3a7bc877
Alex Gurvich <agurvich@u.northwestern.edu>
2019-08-19 14:28:00 -0500

    thanks stack overflow for fixing my 1d_layered_texture!

--- cuda/texture/1d_layered_texture.cu
+++ cuda/texture/1d_layered_texture.cu
@@ -3,128 +3,59 @@
 __global__ void kernelSampleLayeredTexture(
     cudaTextureObject_t tex,
     int width,
-    int height,
     float* normalized_indices,
     int nlayers){
 
     // force threads to execute in a specific order
-    for (int i = 0; i < width; i++){
-        for(int j=0; j < height; j++){
+    for (int layer=0; layer < nlayers; layer++){
+        for (int i = 0; i < width; i++){
             float u = i+0.5;
-            float v = j+0.5;
-            float x = tex2D<float>(tex,u,v);
-            printf("(%.2f, %.2f, %.2f)\n",u-0.5,v-0.5,x);
+            float x = tex1DLayered<float>(tex,u,layer); // last 0 is the 0th layer
+            printf("(%.2f, %.2f)\n",u-0.5,x);
         }
     }
 }
 
-/*
-    myparms.dstArray = cu_3darray;
-    myparms.extent = make_cudaExtent(
-        width,
-        height,
-        num_layers);
-    myparms.kind = cudaMemcpyHostToDevice;
-    checkCudaErrors(cudaMemcpy3D(&myparms));
-
-*/
-
-cudaTextureObject_t simpleLayeredTexture(float * h_data, int width, int height, int num_layers){
+cudaTextureObject_t simpleLayeredTexture(float * h_data, int width, int num_layers){
     /* h_data is flattened array containing all the information... */
     // allocate array and copy image data
     cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc(32, 0, 0, 0, cudaChannelFormatKindFloat);
+    cudaMemcpy3DParms my_params = {0};
+    //my_params.srcPos = make_cudaPos(0,0,0);
+    //my_params.dstPos = make_cudaPos(0,0,0);
+    my_params.srcPtr = make_cudaPitchedPtr(h_data,width *sizeof(float),width,1);
+    my_params.kind = cudaMemcpyHostToDevice;
+    my_params.extent = make_cudaExtent(width, 1, num_layers);
+
+    // create the cuda array and copy the data to it
     cudaArray *cu_3darray;
-    cudaMalloc3DArray(&cu_3darray, &channelDesc, make_cudaExtent(width, height,0)); //num_layers, cudaArrayLayered));
-    cudaMemcpy3DParms myparms = {0};
-    myparms.srcPos = make_cudaPos(0,0,0);
-    myparms.dstPos = make_cudaPos(0,0,0);
-    myparms.srcPtr = make_cudaPitchedPtr(h_data, width * sizeof(float), width, height);
-    myparms.dstArray = cu_3darray;
-    myparms.extent = make_cudaExtent(width, height, 0);//num_layers);
-    myparms.kind = cudaMemcpyHostToDevice;
-    cudaMemcpy3D(&myparms);
+    cudaMalloc3DArray(&cu_3darray, &channelDesc, make_cudaExtent(width, 0,num_layers),cudaArrayLayered);
+    my_params.dstArray = cu_3darray;
+    cudaMemcpy3D(&my_params);
+
+    // Describe the input array
+    cudaResourceDesc            resDesc;
+    memset(&resDesc,0,sizeof(cudaResourceDesc));
+
+    resDesc.resType            = cudaResourceTypeArray;
+    resDesc.res.array.array    = cu_3darray;
+
+    // Describe the output texture
+    cudaTextureDesc             texDesc;
+    memset(&texDesc,0,sizeof(cudaTextureDesc));
+    texDesc.normalizedCoords = false;
+    texDesc.filterMode       = cudaFilterModeLinear;
+    texDesc.addressMode[0] = cudaAddressModeClamp;
+    texDesc.readMode = cudaReadModeElementType;
 
     cudaTextureObject_t         tex;
-    cudaResourceDesc            texRes;
-    memset(&texRes,0,sizeof(cudaResourceDesc));
-
-    texRes.resType            = cudaResourceTypeArray;
-    texRes.res.array.array    = cu_3darray;
-
-    cudaTextureDesc             texDescr;
-    memset(&texDescr,0,sizeof(cudaTextureDesc));
-
-    texDescr.normalizedCoords = false;
-    texDescr.filterMode       = cudaFilterModeLinear;
-    texDescr.addressMode[0] = cudaAddressModeClamp;
-    texDescr.addressMode[1] = cudaAddressModeClamp;
-    texDescr.readMode = cudaReadModeElementType;
-
-    cudaCreateTextureObject(&tex, &texRes, &texDescr, NULL);
-    return tex;
-}
-
-/*
-cudaTextureObject_t make2DLayeredTextureFromPointer(
-    float * arr,
-    int Narr){
-    
-    int height=1;
-    int num_layers=1;
-
-    cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc(
-        32,0,0,0,
-        cudaChannelFormatKindFloat);
-    cudaArray* cuArray;
-    // cudaMallocArray inherently allocates 2D, 
-    //  last argument is 
-    cudaMalloc3DArray(
-        &cuArray,
-        &channelDesc,
-        make_cudaExtent(Narr, height, num_layers));
-        //cudaArrayLayered);
-
-    cudaMemcpy3DParms myparms = {0};
-    myparms.srcPos = make_cudaPos(0,0,0);
-    myparms.dstPos = make_cudaPos(0,0,0);
-    myparms.srcPtr = make_cudaPitchedPtr(
-        arr,
-        Narr*sizeof(float),
-        Narr,
-        height);
-    myparms.dstArray = cuArray;
-    myparms.extent = make_cudaExtent(
-        Narr,
-        height,
-        num_layers);
-    myparms.kind = cudaMemcpyHostToDevice;
-    cudaMemcpy3D(&myparms);
-
-    //create texture object
-    struct cudaResourceDesc resDesc;
-    memset(&resDesc,0,sizeof(resDesc));
-    resDesc.resType = cudaResourceTypeArray;
-    resDesc.res.array.array = cuArray;
-
-    cudaTextureDesc texDesc;
-    memset(&texDesc, 0, sizeof(texDesc));
-    texDesc.addressMode[0] =cudaAddressModeClamp;
-    texDesc.addressMode[1] =cudaAddressModeClamp;
-    texDesc.filterMode = cudaFilterModeLinear;
-    texDesc.readMode = cudaReadModeElementType;
-    texDesc.normalizedCoords = 0;
-
-    // create texture object: we only have to do this once!
-    cudaTextureObject_t tex=0;
     cudaCreateTextureObject(&tex, &resDesc, &texDesc, NULL);
     return tex;
 }
-*/
 
 void sampleTexture(
     cudaTextureObject_t tex,
     int width,
-    int height,
     int nsamples,
     int nlayers){
 
@@ -148,7 +79,6 @@ void sampleTexture(
     kernelSampleLayeredTexture<<<1, 1>>>(
         tex,
         width,
-        height,
         d_normalized_indices,
         nlayers);
 
@@ -179,8 +109,8 @@ int main(){
     //cudaTextureObject_t tex = make2DLayeredTextureFromPointer(
         //data,ntexture_edges);
 
-    cudaTextureObject_t tex = simpleLayeredTexture(data,ntexture_edges/2, 2, 1);
-    sampleTexture(tex,width,height,nsamples,1);
+    cudaTextureObject_t tex = simpleLayeredTexture(data,width, height);
+    sampleTexture(tex,width,nsamples,height);
 
     free(data);
     cudaDestroyTextureObject(tex);
