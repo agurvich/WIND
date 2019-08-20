0110db064bf0e53bfc41658e12cc147964486341
Alex Gurvich <agurvich@u.northwestern.edu>
2019-08-20 13:55:21 -0500

    add routine that creates table in structure_test

--- cuda/structures/struct_test.cu
+++ cuda/structures/struct_test.cu
@@ -13,11 +13,11 @@ extern struct Table_struct{
 __global__ void readStruct(int x, struct Table_struct * p_table){
     struct Table_struct this_table = *p_table;
     int N_reactions_all = this_table.N_reactions[1];
+    printf("%d Nreactions\n",N_reactions_all);
     // loop over all the equations in parallel, with whatever threads we were given..
     int idx;
     for (int rxn_loop_i=0; rxn_loop_i < (N_reactions_all/blockDim.x+1); rxn_loop_i++){
         idx = threadIdx.x+rxn_loop_i*blockDim.x;
-
         if (idx < N_reactions_all){
             float this_rate_coeff = this_table.rates[idx];
             printf("%.2f ",this_rate_coeff);
@@ -26,13 +26,63 @@ __global__ void readStruct(int x, struct Table_struct * p_table){
                     this_table.reactantss_transpose_flat[idx+reactant_i*N_reactions_all]);
             }// for react_i
         }
-
     }// for rxn_loop_i
-        
 }
 
 // create a new global table
 struct Table_struct table;
+
+void initialize_table(
+    struct Table_struct * p_this_table,
+    int * N_reactions,
+    int * reactantss_transpose_flat,
+    int * productss_transpose_flat,
+    int H2_collis_dissoc_heating_reaction_index,
+    int H2_form_heating_reaction_index,
+    float * rates){
+
+    // bind the simple stuff that's already allocated in the struct
+    p_this_table->N_reactions[0] = N_reactions[0];
+    p_this_table->N_reactions[1] = N_reactions[1];
+    p_this_table->H2_collis_dissoc_heating_reaction_index=H2_collis_dissoc_heating_reaction_index;
+    p_this_table->H2_form_heating_reaction_index=H2_form_heating_reaction_index; 
+
+    printf(" n reactions is %d %d \n",p_this_table->N_reactions[0],p_this_table->N_reactions[1]);
+
+    // allocate and copy the reactants over, then bind to the host this_table structure
+    int * d_reactantss_transpose_flat;
+    cudaMalloc(&d_reactantss_transpose_flat,
+        sizeof(int)*3*p_this_table->N_reactions[1]);
+    cudaMemcpy(d_reactantss_transpose_flat,
+        reactantss_transpose_flat,
+        sizeof(int)*3*p_this_table->N_reactions[1],
+        cudaMemcpyHostToDevice);
+    p_this_table->reactantss_transpose_flat = d_reactantss_transpose_flat; // needs to be a device array
+
+    // allocate and copy the products over, then bind to the host this_table structure
+    int * d_productss_transpose_flat;
+    cudaMalloc(
+        &d_productss_transpose_flat,
+        sizeof(int)*3*p_this_table->N_reactions[1]);
+    cudaMemcpy(
+        d_productss_transpose_flat,
+        productss_transpose_flat,
+        sizeof(int)*3*p_this_table->N_reactions[1],
+        cudaMemcpyHostToDevice);
+    p_this_table->productss_transpose_flat = d_productss_transpose_flat; // needs to be a device array
+    // allocate and copy the rates over, then bind to the host table structure
+
+    float * d_rates;
+    cudaMalloc(
+        &d_rates,
+        sizeof(float)*p_this_table->N_reactions[1]);
+    cudaMemcpy(
+        d_rates,
+        rates,
+        sizeof(float)*p_this_table->N_reactions[1],
+        cudaMemcpyHostToDevice);
+    p_this_table->rates = d_rates; // needs to be a device array
+}
 
 int main(){
     // initialize fake host data
@@ -46,47 +96,17 @@ int main(){
 
     int num_species = 5;
 
-    // bind the simple stuff that's already allocated in the struct
-    table.N_reactions[0] = N_reactions[0];
-    table.N_reactions[1] = N_reactions[1];
-    table.H2_collis_dissoc_heating_reaction_index=H2_collis_dissoc_heating_reaction_index;
-    table.H2_form_heating_reaction_index=H2_form_heating_reaction_index; 
 
-    printf(" n reactions is %d %d \n",table.N_reactions[0],table.N_reactions[1]);
-
-    // allocate and copy the reactants over, then bind to the host table structure
-    int * d_reactantss_transpose_flat;
-    cudaMalloc(&d_reactantss_transpose_flat,
-        sizeof(int)*3*table.N_reactions[1]);
-    cudaMemcpy(d_reactantss_transpose_flat,
+    // add N_reactions
+    initialize_table(
+        &table,
+        N_reactions,
         reactantss_transpose_flat,
-        sizeof(int)*3*table.N_reactions[1],
-        cudaMemcpyHostToDevice);
-    table.reactantss_transpose_flat = d_reactantss_transpose_flat; // needs to be a device array
-
-    // allocate and copy the products over, then bind to the host table structure
-    int * d_productss_transpose_flat;
-    cudaMalloc(
-        &d_productss_transpose_flat,
-        sizeof(int)*3*table.N_reactions[1]);
-    cudaMemcpy(
-        d_productss_transpose_flat,
         productss_transpose_flat,
-        sizeof(int)*3*table.N_reactions[1],
-        cudaMemcpyHostToDevice);
-    table.productss_transpose_flat = d_productss_transpose_flat; // needs to be a device array
-
-    // allocate and copy the rates over, then bind to the host table structure
-    float * d_rates;
-    cudaMalloc(
-        &d_rates,
-        sizeof(float)*table.N_reactions[1]);
-    cudaMemcpy(
-        d_rates,
-        rates,
-        sizeof(float)*table.N_reactions[1],
-        cudaMemcpyHostToDevice);
-    table.rates = d_rates; // needs to be a device array
+        H2_collis_dissoc_heating_reaction_index,
+        H2_form_heating_reaction_index,
+        rates); 
+    printf(" n reactions is %d %d \n",table.N_reactions[0],table.N_reactions[1]);
 
     // allocate a table on the device, copy over the host structure over
     struct Table_struct * d_p_table;
